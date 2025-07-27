f03e54b71606743e021d6f8ced6e6cb909211036
JackChmel <jack.chmel@gmail.com>
2025-07-27 22:09:07 +0200

    Update WolfChunkLoader.java

--- wolfchunk_loader/src/main/java/com/example/wolfchunkloader/WolfChunkLoader.java
+++ wolfchunk_loader/src/main/java/com/example/wolfchunkloader/WolfChunkLoader.java
@@ -2,7 +2,7 @@ package com.example.wolfchunkloader;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod("wolfchunk_loader")
+@Mod("wolfchunkloader")
 public class WolfChunkLoader {
     public WolfChunkLoader() {
         System.out.println("WolfChunkLoader mod byl úspěšně načten!");
